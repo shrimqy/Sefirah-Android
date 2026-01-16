b9d4ccac90715916b68fc5bda6a163f28def071f
shrimqy <shrimqy@gmail.com>
2026-01-16 19:02:42 +0530

    fix: do not show media session for sessions which wasn't updated

--- feature/projection/src/main/java/sefirah/projection/media/MediaHandler.kt
+++ feature/projection/src/main/java/sefirah/projection/media/MediaHandler.kt
@@ -126,7 +126,6 @@ class MediaHandler @Inject constructor(
                     showMediaSession(deviceId, updatedSession)
                     updatedSession
                 } else {
-                    showMediaSession(deviceId, session)
                     session
                 }
             }
